d84a374580c88e2b410d4dd68d0e5fd64f7e1345
goldipl <mgodlewski92@gmail.com>
2023-07-08 22:07:33 +0200

    handle mouseover and mouseout while hovering menu

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const bottom_nav_link = document.querySelectorAll('#nav .bottom-nav .bottom-nav-container .left-bottom-nav ul.bottom-nav-elements-container li.bottom-nav-links');
+
+bottom_nav_link.forEach(e => {
+    e.addEventListener('mouseover', () => {
+        document.body.classList.add('menu-hover');
+    });
+    e.addEventListener('mouseout', () => {
+        document.body.classList.remove('menu-hover');
+    });
+})
