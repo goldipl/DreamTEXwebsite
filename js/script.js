7f656c0801f8b857af24c349fc7de1809387cdeb
goldipl <mgodlewski92@gmail.com>
2023-07-10 20:24:15 +0200

    handle chevron up btn

--- js/script.js
+++ js/script.js
@@ -12,3 +12,7 @@ bottom_nav_link.forEach(e => {
 const footer_year = document.querySelector('footer .footer_container_bottom-left span');
 
 footer_year.innerHTML = new Date().getFullYear();
+
+const footer_arrow_up = document.querySelector('footer .footer-arrow-up');
+
+footer_arrow_up.addEventListener('click', () => window.scrollTo(0,0))
