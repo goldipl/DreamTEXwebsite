54f93c72170de426c4694d3c175405f9776656a7
goldipl <mgodlewski92@gmail.com>
2023-07-11 15:07:51 +0200

    handle messenger

--- js/script.js
+++ js/script.js
@@ -16,3 +16,8 @@ footer_year.innerHTML = new Date().getFullYear();
 const footer_arrow_up = document.querySelector('footer .footer-arrow-up');
 
 footer_arrow_up.addEventListener('click', () => window.scrollTo(0,0))
+
+const messenger = document.querySelector('.messenger');
+const messe_x_icon = document.querySelector('.messenger .messenger-x-icon');
+
+messe_x_icon.addEventListener('click', () => messenger.style.display = "none");
