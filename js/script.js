48f5854e25b0e7baf8e878b684fbcf242b3a1203
goldipl <mgodlewski92@gmail.com>
2023-07-17 15:08:41 +0200

    handle mobile navigation

--- js/script.js
+++ js/script.js
@@ -21,3 +21,34 @@ const messenger = document.querySelector('.messenger');
 const messe_x_icon = document.querySelector('.messenger .messenger-x-icon');
 
 messe_x_icon.addEventListener('click', () => messenger.style.display = "none");
+
+const hamburger = document.querySelector('#mobile-nav .mobile-nav-container .mobile-nav-container-slot.hamburger .hamburger-container');
+const mobile_nav_menu = document.querySelector('#mobile-nav .mobile-nav-menu-items');
+
+hamburger.addEventListener('click', () => {
+    mobile_nav_menu.classList.toggle('open');
+    hamburger.classList.toggle('active');
+});
+
+const mobile_nav_list_item = document.querySelectorAll('#mobile-nav .mobile-nav-menu-items .mobile-nav-list .mobile-nav-list-items-icons .mobile-nav-list-item-icon');
+
+mobile_nav_list_item.forEach(e => {
+    e.addEventListener('click', () => {
+        e.classList.toggle('active');
+    });
+})
+
+const mobile_nav_close_icon = document.querySelectorAll('#mobile-nav .mobile-nav-sublist-close-icon');
+
+mobile_nav_close_icon.forEach(e => {
+    e.addEventListener('click', () => {
+        mobile_nav_menu.classList.toggle('open');
+        hamburger.classList.toggle('active');
+    });
+});
+
+const mobile_nav_back_icon = document.querySelector('#mobile-nav .mobile-nav-sublist-title .mobile-back-icon');
+
+mobile_nav_back_icon.addEventListener('click', () => {
+    mobile_nav_back_icon.parentElement.parentElement.classList.toggle('active');
+})
