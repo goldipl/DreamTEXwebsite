112d85d36d2c9a6c214b6a44674604952e250ce7
goldipl <mgodlewski92@gmail.com>
2023-07-20 21:21:52 +0200

    handle courier cart delivery

--- js/cart-delivery.js
+++ js/cart-delivery.js
@@ -1,5 +1,11 @@
 const delivery_square = document.querySelectorAll('#cart_delivery_payment .cart_delivery_payment_container .cart_delivery_payment_container-slot .cart_square_container .cart_square-slot');
+const courier_address = document.querySelector('#cart_delivery_payment .cart_delivery_payment_container .cart_delivery_payment_container-slot .courier_address_container');
 
 delivery_square.forEach(e => e.addEventListener('click', () => {
     e.classList.toggle('checked');
+    if (e.classList.contains('checked') && e.classList.contains('courier')) {
+        courier_address.classList.add('active');
+    } else {
+        courier_address.classList.remove('active');
+    }
 }));
