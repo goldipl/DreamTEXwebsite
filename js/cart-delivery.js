8d8de86e7bb987faf33717693c95bd72befd6f7d
goldipl <mgodlewski92@gmail.com>
2023-07-20 21:52:15 +0200

    fix cart deliver js picker

--- js/cart-delivery.js
+++ js/cart-delivery.js
@@ -5,7 +5,14 @@ delivery_square.forEach(e => e.addEventListener('click', () => {
     e.classList.toggle('checked');
     if (e.classList.contains('checked') && e.classList.contains('courier')) {
         courier_address.classList.add('active');
+        if ((e.classList.contains('checked'))) {
+            e.nextElementSibling.classList.remove('checked');
+            e.previousElementSibling.classList.remove('checked');
+        }
     } else {
         courier_address.classList.remove('active');
+        if ((e.classList.contains('checked'))) {
+            e.previousElementSibling.classList.remove('checked');
+        }
     }
 }));
