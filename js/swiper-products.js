c2b2836009d5f049331e3948ce81636e544221cc
goldipl <mgodlewski92@gmail.com>
2023-07-27 13:09:13 +0200

    fix products swiper group settings

--- js/swiper-products.js
+++ js/swiper-products.js
@@ -2,6 +2,8 @@ const swiper_products = new Swiper('.swiper-products', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
+    slidesPerView: 2,
+    slidesPerGroup: 2,
   
     // If we need pagination
     pagination: {
@@ -13,5 +15,19 @@ const swiper_products = new Swiper('.swiper-products', {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
+
+    breakpoints: {
+      // when window width is >= 960px
+      960: {
+        slidesPerView: 2,
+        slidesPerGroup: 2,
+      },
+      // when window width is >= 200px
+      200: {
+        slidesPerView: 1,
+        slidesPerGroup: 1,
+      },
+    },
+    
   
   });
