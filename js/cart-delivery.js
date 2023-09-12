a3cde370d42589787f0e45b7abb08c21e5ddd8d9
goldipl <mgodlewski92@gmail.com>
2023-09-12 20:06:37 +0200

    add and handle new delivery switch

--- js/cart-delivery.js
+++ js/cart-delivery.js
@@ -1,16 +1,24 @@
 const delivery_square = document.querySelectorAll('#cart_delivery_payment .cart_delivery_payment_container .cart_delivery_payment_container-slot .cart_square_container:not(.payment-method) .cart_square-slot');
 const courier_address = document.querySelector('#cart_delivery_payment .cart_delivery_payment_container .cart_delivery_payment_container-slot .courier_address_container');
+const delivery_switch = document.querySelector('#cart_delivery_payment .cart_delivery_payment_container .cart_delivery_payment_container-slot .title-checkbox .checkbox-wrapper-64 .switch');
+const input_switch = document.querySelector('#cart_delivery_payment .cart_delivery_payment_container .cart_delivery_payment_container-slot .title-checkbox .checkbox-wrapper-64 input')
+
+delivery_switch.addEventListener('click', () => {
+    if (input_switch.checked) {
+        courier_address.classList.add('active');
+    } else {
+        courier_address.classList.remove('active');  
+    }
+});
 
 delivery_square.forEach(e => e.addEventListener('click', () => {
     e.classList.toggle('checked');
     if (e.classList.contains('checked') && e.classList.contains('courier')) {
-        courier_address.classList.add('active');
         if ((e.classList.contains('checked'))) {
             e.nextElementSibling.classList.remove('checked');
             e.previousElementSibling.classList.remove('checked');
         }
     } else {
-        courier_address.classList.remove('active');
         if ((e.classList.contains('checked'))) {
             e.previousElementSibling.classList.remove('checked');
         }
