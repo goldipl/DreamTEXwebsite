e62f30100491c84b3cf83971636accd555cd93fb
goldipl <mgodlewski92@gmail.com>
2023-09-16 18:47:34 +0200

    handle toggling class active on all selected variants

--- js/colors_variants.js
+++ js/colors_variants.js
@@ -2,5 +2,12 @@ const color = document.querySelector('#products-catalog .products_catalog_contai
 const variants = document.querySelector('#products-catalog .products_catalog_container .filters_container .filters_column .accordion.variants');
 
 color.addEventListener('click', () => {
+    color.classList.toggle('active');
     variants.classList.toggle('show');
 });
+
+colors = document.querySelectorAll('#products-catalog .products_catalog_container .filters_container .filters_column .slot .filters-colors .color');
+
+colors.forEach(e => e.addEventListener('click', () => {
+    e.classList.toggle('active');
+}))
